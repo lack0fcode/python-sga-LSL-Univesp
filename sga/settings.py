363a98ff6f89053536e843ac0e3945adf4c139cb
Caue Felipe Trovatto Tragante <cauef.t.tragante@gmail.com>
2025-08-25 10:38:45 -0300

    Update settings.py

--- sga/settings.py
+++ sga/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-61hqaf2qqt(6!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
