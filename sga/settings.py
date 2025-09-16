fef0764c133f50fdc2d190f200e8fd2c45c931ac
Caue Felipe Trovatto Tragante <cauef.t.tragante@gmail.com>
2025-09-16 16:21:28 -0300

    Update DATABASES configuration in settings.py
    Removed SSL requirement from database connection settings.

--- sga/settings.py
+++ sga/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'sga.wsgi.application'
 DATABASE_URL = os.environ.get("DATABASE_URL") or config("DATABASE_URL")
 
 DATABASES = {
-    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    "default": dj_database_url.parse(DATABASE_URL)
 }
 
 # Password validation
